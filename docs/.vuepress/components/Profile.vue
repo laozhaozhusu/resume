8ff762712c192996da7ee5fd75abef145fb97eeb
Xipeng Zhao <xipeng.zhao.china@gmail.com>
2025-03-27 14:21:48 +0800

    init

---
+++ docs/.vuepress/components/Profile.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="profile">
+    <h1>赵希鹏</h1>
+    <div>
+      <span>全日制本科</span
+      >|<span>武汉</span>|<span>电话:13016491758</span>|<span
+        >邮箱:291704950@qq.com</span
+      >
+    </div>
+    <div>
+      <span>8 年工作经验</span>|<span
+        >求职意向：<strong>全职前端开发</strong></span
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Profile",
+};
+</script>
+
+<style scoped>
+.profile {
+  display: flex;
+  justify-items: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  base: "/resume/",
+  themeConfig: {
+    // 禁用搜索
+    search: false,
+    // 不需要导航栏
+    navbar: false,
+  },
+};
