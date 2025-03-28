fb43919bc4180dcb38ffe01142d7a5ddb2e3dc62
Xipeng Zhao <xipeng.zhao.china@gmail.com>
2025-03-28 15:24:16 +0800

    更新简历内容

---
+++ docs/.vuepress/components/CompanyProfile.vue
@@ -0,0 +1,27 @@
+<template>
+    <el-row :gutter="20" justify="space-between">
+      <el-col :span="8"
+        ><strong :style="{ fontWeight: '700' }">{{ name }}</strong></el-col
+      >
+      <el-col :span="8" :style="{ textAlign: 'center' }"
+        ><strong :style="{ fontWeight: '700' }">{{ title }}</strong></el-col
+      >
+      <el-col :span="8" :style="{ textAlign: 'right' }"
+        ><strong :style="{ fontWeight: '700' }">{{ workTime }}</strong></el-col
+      >
+    </el-row>
+  </template>
+  
+  <script>
+  export default {
+    name: "CompanyProfile",
+    props: {
+      name: String,
+      title: String,
+      department: String,
+      workTime: String,
+    },
+  };
+  </script>
+  
+  <style scoped></style>
